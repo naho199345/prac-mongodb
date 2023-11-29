aea5233260680e99b4de5ed511efb9d2ad626eae
김승호 <naho199345@naver.com>
2023-11-30 00:46:40 +0900

    create: Create API for updating user information

--- src/router/users.js
+++ src/router/users.js
@@ -48,4 +48,40 @@ router.delete("/user/:userId", async (req, res) => {
   }
 });
 
+router.put("/user/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    if (!mongoose.isValidObjectId(userId))
+      return res.status(400).send({ err: "invalid userId" });
+    const { age, name } = req.body;
+    if (!age && !name)
+      return res.status(400).send({ err: "age or name is required" });
+    if (age && typeof age !== "number")
+      return res.status(400).send({ err: "age must be a number" });
+    if (
+      name &&
+      typeof name.firstName !== "string" &&
+      typeof name.lastName !== "number"
+    )
+      return res.status(400).send({ err: "first and last name is required" });
+    // 수정할 사항이 단순할 때, 1번 호출이 되서 효율
+    // let updateBody = {};
+    // if (age) updateBody.age = age;
+    // if (name) updateBody.name = name;
+    // const user = await User.findByIdAndUpdate(userId, updateBody, {
+    //   new: true,
+    // });
+
+    // 수정할 사항이 복잡할 때, 2번 호출이 되서 비효율
+    let user = await User.findById(userId);
+    if (age) user.age = age;
+    if (name) user.name = name;
+    await user.save();
+    return res.send({ user });
+  } catch {
+    console.log(err);
+    return res.status(500).send({ err: err.message });
+  }
+});
+
 module.exports = router;
